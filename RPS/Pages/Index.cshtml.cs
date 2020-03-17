0403eb509dcb031e95b8222e013900274dd0bc80
Petr Dědic <petr.dedic@pslib.cz>
2020-03-17 14:41:05 +0100

    Ready for improvement

---
+++ RPS/Pages/Index.cshtml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RPS.Model;
+
+namespace RPS.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public Choice Player { get; set; }
+        public Choice Computer { get; set; }
+        [BindProperty]
+        public int Round { get; set; }
+        [BindProperty]
+        public int Win { get; set; }
+        [BindProperty]
+        public int Draw { get; set; }
+        [BindProperty]
+        public int Lose { get; set; }
+
+
+        private Random _random;
+
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            _random = new Random();
+        }
+
+        public void OnGet()
+        {
+            Player = Choice.None;
+            Computer = Choice.None;
+            Round = 0;
+            Win = 0;
+            Lose = 0;
+            Draw = 0;
+        }
+
+        public void OnPost() {
+            Computer = (Choice)_random.Next(1, 4);
+            Round++;
+
+            if(Player != Computer)
+            {
+                if ((Player == Choice.Rock && Computer == Choice.Scissors) || (Player == Choice.Scissors && Computer == Choice.Paper) || (Player == Choice.Paper && Computer == Choice.Rock)) {
+                    Win++;
+                }
+                else
+                {
+                    Lose++;
+                }
+            }
+            else
+            {
+                Draw++;
+            }
+        }
+    }
+}
