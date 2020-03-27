51b901f23f4fcd613c14f3f990c294796bc5d3f3
Petr Dědic <petr.dedic@pslib.cz>
2020-03-27 08:08:38 +0100

    https://github.com/PetrDedic/RPS
    Kopírování z repozitáře

---
+++ RPS/Model/Rps.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.Model
+{
+    public class Rps
+    {
+        public bool? IsWinner { get; set; }
+        public int WinCounter { get; set; }
+        public int LossCounter { get; set; }
+        public int DrawCounter { get; set; }
+        public RpsMode AiChoice { get; set; }
+        public RpsMode UserChoice { get; set; }
+
+    }
+}
---
+++ RPS/Services/RpsLogic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RockPaperScissors.Model;
+
+namespace RockPaperScissors.Services
+{
+    public class RpsLogic
+    {
+        private readonly Random random;
+        public RpsLogic(Random random)
+        {
+            this.random = random;
+            userData = new Rps();
+        }
+
+        public Rps userData { get; set; }
+
+        public void GameRound(RpsMode userChoice)
+        {
+            if (userChoice == RpsMode.None) return;
+            RpsMode aiChoice = (RpsMode)random.Next(1, 4);
+            userData.AiChoice = aiChoice;
+            userData.UserChoice = userChoice;
+
+            if (aiChoice == RpsMode.Rock && userChoice == RpsMode.Paper ||
+                aiChoice == RpsMode.Paper && userChoice == RpsMode.Scissors ||
+                aiChoice == RpsMode.Scissors && userChoice == RpsMode.Rock)
+            {
+                userData.WinCounter++;
+                userData.IsWinner = true;
+            }
+
+            else if (aiChoice == userChoice)
+            {
+                userData.DrawCounter++;
+                userData.IsWinner = null;
+            }
+            else
+            {
+                userData.LossCounter++;
+                userData.IsWinner = false;
+            }
+        }
+    }
+}
